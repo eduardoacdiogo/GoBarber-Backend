ed88cd01baf97cb15225ae202eb14aacb721fab3
Eduardo Andre <educdiogo@hotmail.com>
2022-03-26 10:31:00 -0300

    list providers

---
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import ensureAuthenticate from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import ProvidersController from '../controllers/ProvidersController';
+
+const providersRouter = Router();
+const providersController = new ProvidersController();
+
+providersRouter.use(ensureAuthenticate);
+providersRouter.post('/', providersController.index);
+
+export default providersRouter;
